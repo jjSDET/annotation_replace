0ad213d19aa1599ec38eee26b822727aa9f232fb
Jackie Johnson <107960801+jjSDET@users.noreply.github.com>
2023-09-22 12:32:59 -0500

    add initial requirements

---
+++ annotation_helper.py
@@ -0,0 +1,35 @@
+'''
+ref: https://www.phind.com/search?cache=lfcenythj224kph8dfxuorv1
+
+WORKFLOW:
+    Entrypoint/trigger:
+
+
+REQUIREMENTS:
+
+V 0.0.1
+    When adding import Ignore, must be placed lexical order
+        else linter will fail
+    When removing contents, no whitespace should be left
+        or carriage returns
+    No other modifications should be made to file except add/remove
+        of annotations or import Ignore
+    Accept single request initially for add/remove content
+
+ANDROID MONO-REPO:
+    search by arg focus, fenix, android-components
+    get all esspresso test '.kt's files containing test_name
+    add or remove file contents:
+        case for removal:
+        
+        import org.junit.Ignore
+            if no @Ignores remain in file
+        @Ignore
+            
+        case for adding:
+        
+iOS:
+    things to consider...
+'''
+
+
